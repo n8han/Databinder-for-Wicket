16c2a0e265f2c563bb87cf3f74b611e672c68b70
Rodolfo Hansen <kryptt@gmail.com>
2010-02-02 20:31:24 -0400

    Allow extending the idea of clearing the default search model

--- databinder-components-hib/src/main/java/net/databinder/components/hib/SearchPanel.java
+++ databinder-components-hib/src/main/java/net/databinder/components/hib/SearchPanel.java
@@ -25,95 +25,102 @@ import org.hibernate.criterion.Restrictions;
 /**
  * Panel for a "live" search field with a clear button. Instances of this class must
  * implement the onUpdate method to register external components for updating.
- * It is possible to override the search button text with the key "searchbutton.text" 
+ * It is possible to override the search button text with the key "searchbutton.text"
  * The SearchPanel model maps to the text of the search.
  * @author Nathan Hamblen
  */
 public abstract class SearchPanel extends Panel {
-	
+
 	private TextField search;
-	
+
 	/**
 	 * @param id Wicket id
 	 */
-	public SearchPanel(String id) {
+	public SearchPanel(final String id) {
 		super(id, new Model());
 		add(new SearchForm("searchForm"));
 	}
 
 	/** Use the given model (must not be read-only ) for the search string */
-	public SearchPanel(String id, IModel searchModel) {
+	public SearchPanel(final String id, final IModel searchModel) {
 		super(id, searchModel);
 		add(new SearchForm("searchForm"));
 	}
-	
+
 	@Override
 	/** Sets model to search component. */
-	public MarkupContainer setDefaultModel(IModel model) {
+	public MarkupContainer setDefaultModel(final IModel model) {
 		return search.setDefaultModel(model);
 	}
-	
+
 	/**
 	 * Override to add components to be updated (or JavaScript to be executed)
 	 * when the search string changes. Remember that added components must
-	 * have a markup id; use component.setMarkupId(true) to assign one 
+	 * have a markup id; use component.setMarkupId(true) to assign one
 	 * programmatically.
 	 * @param target Ajax target to register components for update
 	 */
 	public abstract void onUpdate(AjaxRequestTarget target);
-	
+
 	/**
-	 * Binds the search model to a "search" parameter in a query. The value in the 
+	 * Binds the search model to a "search" parameter in a query. The value in the
 	 * search field will be bracketed by percent signs (%) for a find-anywhere match.
-	 * In the query itself, "search" must be the name of the  one and only parameter 
-	 * If your needs differ, bind the model passed in to the SearchPanel constructor 
-	 * to your own IQueryBinder instance; this is a convenience method. 
+	 * In the query itself, "search" must be the name of the  one and only parameter
+	 * If your needs differ, bind the model passed in to the SearchPanel constructor
+	 * to your own IQueryBinder instance; this is a convenience method.
 	 * @return binder for a "search" parameter
 	 */
 	public QueryBinder getQueryBinder() {
 		return new PropertyQueryBinder(this);
 	}
-	
+
 	/**
 	 * Adds a criterion that will match the current search string anywhere within
 	 * any of the given properties. If the search is empty, no criterion is added.
 	 * @param searchProperty one or more properties to be searched
-	 * @return builder to be used with list model or data provider 
+	 * @return builder to be used with list model or data provider
 	 */
 	public CriteriaBuilder getCriteriaBuilder(final String... searchProperty) {
 		return getCriteriaBuilder(MatchMode.ANYWHERE, searchProperty);
 	}
-	
+
 	/**
 	 * Adds a criterion that will match the current search string within (depending on the MatchMode)
 	 * any of the given properties. If the search is empty, no criterion is added.
-	 * @param matchMode used against all properties 
+	 * @param matchMode used against all properties
 	 * @param searchProperty one or more properties to be searched
-	 * @return builder to be used with list model or data provider 
+	 * @return builder to be used with list model or data provider
 	 */
 	public CriteriaBuilder getCriteriaBuilder(final MatchMode matchMode, final String... searchProperty) {
 		return new CriteriaBuilder() {
-			public void build(Criteria criteria) {
-				String search = (String) getDefaultModelObject();
+			public void build(final Criteria criteria) {
+				final String search = (String) getDefaultModelObject();
 				if (search != null) {
-					Disjunction d = Restrictions.disjunction();
-					for (String prop : searchProperty)
-						d.add(Property.forName(prop).like(search, matchMode));
+					final Disjunction d = Restrictions.disjunction();
+					for (final String prop : searchProperty) {
+            d.add(Property.forName(prop).like(search, matchMode));
+          }
 					criteria.add(d);
 				}
 			}
 		};
-	}	
-	
+	}
+
 	/** @return search string bracketed by the % wildcard */
 	public String getSearch() {
 		return getDefaultModelObject() == null ? null : "%" + getDefaultModelObject() + "%";
 	}
-	
-	/** Form with AJAX components and their AjaxCells. */
+
+	/** Resets the search model, The default behavior is setting it to null,
+	 * but it is possible to override with your own custom defaults. */
+  public void resetSearchModelObject() {
+    setDefaultModelObject(null);
+  }
+
+  /** Form with AJAX components and their AjaxCells. */
 	public class SearchForm extends Form {
 		@SuppressWarnings("unchecked")
-		public SearchForm(String id) {
+		public SearchForm(final String id) {
 			super(id);
 
 			final AjaxCell searchWrap = new AjaxCell("searchWrap");
@@ -126,25 +133,28 @@ public abstract class SearchPanel extends Panel {
 			add(clearWrap);
 			final AjaxLink clearLink = new AjaxLink("clearLink") {
 				/** Clear field and register updates. */
-				public void onClick(AjaxRequestTarget target) {
-					SearchPanel.this.setDefaultModelObject(null);
+				@Override
+        public void onClick(final AjaxRequestTarget target) {
+					resetSearchModelObject();
 					target.addComponent(searchWrap);
 					target.addComponent(clearWrap);
-					SearchPanel.this.onUpdate(target);
+					onUpdate(target);
 				}
 				/** Hide when search is blank. */
-				public boolean isVisible() {
+				@Override
+        public boolean isVisible() {
 					return SearchPanel.this.getDefaultModelObject() != null;
 				}
 			};
 			clearLink.setOutputMarkupId(true);
-			clearLink.add( new Image("clear", 
+			clearLink.add( new Image("clear",
 					new ResourceReference(this.getClass(), "clear.png")));
 			clearWrap.add(clearLink);
 
 			// triggered when user pauses or tabs out
 			search.add(new AjaxOnKeyPausedUpdater() {
-				protected void onUpdate(AjaxRequestTarget target) {
+				@Override
+        protected void onUpdate(final AjaxRequestTarget target) {
 					target.addComponent(clearWrap);
 					SearchPanel.this.onUpdate(target);
 				}
